39dabe65ccded9cd62e33a3ba6d207cf020636c1
Alexander Zinchuk <alx.zinchuk@gmail.com>
2022-11-05 15:00:46 +0100

    TeactN: Support detaching containers

--- src/teact/teactn.tsx
+++ src/teact/teactn.tsx
@@ -32,7 +32,10 @@ type ActionHandler = (
   payload: any,
 ) => GlobalState | void | Promise<void>;
 
-type MapStateToProps<OwnProps = undefined> = ((global: GlobalState, ownProps: OwnProps) => AnyLiteral);
+type DetachWhenChanged = (current: any) => void;
+type MapStateToProps<OwnProps = undefined> = (
+  (global: GlobalState, ownProps: OwnProps, detachWhenChanged: DetachWhenChanged) => AnyLiteral
+);
 
 let currentGlobal = {} as GlobalState;
 
@@ -52,6 +55,9 @@ const containers = new Map<string, {
   mappedProps?: Props;
   forceUpdate: Function;
   areMappedPropsChanged: boolean;
+  isDetached: boolean;
+  detachReason: any;
+  detachWhenChanged: DetachWhenChanged;
   DEBUG_updates: number;
   DEBUG_componentName: string;
 }>();
@@ -139,13 +145,21 @@ function updateContainers() {
   // eslint-disable-next-line no-restricted-syntax
   for (const container of containers.values()) {
     const {
-      mapStateToProps, ownProps, mappedProps, forceUpdate,
+      mapStateToProps, ownProps, mappedProps, forceUpdate, isDetached, detachWhenChanged,
     } = container;
+
+    if (isDetached) {
+      continue;
+    }
 
     let newMappedProps;
 
     try {
-      newMappedProps = mapStateToProps(currentGlobal, ownProps);
+      newMappedProps = mapStateToProps(currentGlobal, ownProps, detachWhenChanged);
+
+      if (container.isDetached) {
+        continue;
+      }
     } catch (err: any) {
       handleError(err);
 
@@ -236,6 +250,18 @@ export function withGlobal<OwnProps extends AnyLiteral>(
           ownProps: props,
           areMappedPropsChanged: false,
           forceUpdate,
+          isDetached: false,
+          detachReason: undefined,
+          // This allows to ignore changes in global during animation before unmount
+          detachWhenChanged: (current) => {
+            const { detachReason } = container!;
+
+            if (detachReason === undefined && current !== undefined) {
+              container!.detachReason = current;
+            } else if (detachReason !== undefined && detachReason !== current) {
+              container!.isDetached = true;
+            }
+          },
           DEBUG_updates: 0,
           DEBUG_componentName: Component.name,
         };
@@ -251,7 +277,7 @@ export function withGlobal<OwnProps extends AnyLiteral>(
         container.ownProps = props;
 
         try {
-          container.mappedProps = mapStateToProps(currentGlobal, props);
+          container.mappedProps = mapStateToProps(currentGlobal, props, container.detachWhenChanged);
         } catch (err: any) {
           handleError(err);
         }
@@ -298,7 +324,8 @@ export function typify<ProjectGlobalState, ActionPayloads, NonTypedActionNames e
       handler: ActionHandlers[ActionName],
     ) => void,
     withGlobal: withGlobal as <OwnProps extends AnyLiteral>(
-      mapStateToProps: ((global: ProjectGlobalState, ownProps: OwnProps) => AnyLiteral),
+      mapStateToProps: (
+        (global: ProjectGlobalState, ownProps: OwnProps, detachWhenChanged: DetachWhenChanged) => AnyLiteral),
     ) => (Component: FC) => FC<OwnProps>,
   };
 }
